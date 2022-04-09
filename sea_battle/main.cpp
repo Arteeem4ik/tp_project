699abb9ddef781a282a0716bd11e4c729edccd49
Arteeem4ik <artem.akhmedov2003@gmail.com>
2022-04-09 17:48:54 +0300

    Updated README.md and added project files

---
+++ sea_battle/main.cpp
@@ -0,0 +1,6 @@
+#include "game.h"
+
+int main() {
+  Game sea_battle;
+  sea_battle.SeaBattle();
+}
---
+++ sea_battle/square.h
@@ -0,0 +1,10 @@
+#ifndef TP_SEA_BATTLE__SQUARE_H_
+#define TP_SEA_BATTLE__SQUARE_H_
+#include "string"
+
+class Square {
+ public:
+  std::string condition = "empty";
+};
+
+#endif //TP_SEA_BATTLE__SQUARE_H_
