602355bad4a2b44a65e7c66b266442fedf7626a3
Arteeem4ik <artem.akhmedov2003@gmail.com>
2022-04-27 01:04:42 +0300

    Updated player.cpp

--- sea_battle/src/player.cpp
+++ sea_battle/src/player.cpp
@@ -26,21 +26,21 @@ void Player::Arrangement(int length) {
     std::cin >> str;
     if (length == 1) {
       if (str[0] < 'A' || str[0] > 'H' || str[1] < '1' || str[1] > '8') {
-        Interface::PrintIncorrectArrangement(str);
+        interface.PrintIncorrectArrangement(str);
         continue;
       }
       if (IsCorrect(str[1] - '0', str[0] - 64, field)) {
         field.own_field[str[1] - '0'][str[0] - 64].condition = "ship";
         correct = false;
       } else {
-        Interface::PrintIncorrectArrangement(str);
+        interface.PrintIncorrectArrangement(str);
       }
     } else if (str[0] == str[3]) {
       correct = false;
       if (length == 2 && str[4] - str[1] != 1 || length == 3 && str[4] - str[1] != 2
       || length == 4 && str[4] - str[1] != 3 || str[1] < '1' || str[1] > '8' || str[0] < 'A' || str[0] > 'H'
       || str[4] < '1' || str[4] > '8') {
-        Interface::PrintIncorrectArrangement(str);
+        interface.PrintIncorrectArrangement(str);
         correct = true;
         continue;
       }
@@ -48,7 +48,7 @@ void Player::Arrangement(int length) {
         if (IsCorrect(str[1] - '0' + i, str[0] - 64, old_field)) {
           field.own_field[str[1] - '0' + i][str[0] - 64].condition = "ship";
         } else {
-          Interface::PrintIncorrectArrangement(str);
+          interface.PrintIncorrectArrangement(str);
           field = old_field;
           correct = true;
           break;
@@ -59,7 +59,7 @@ void Player::Arrangement(int length) {
       if (length == 2 && str[3] - str[0] != 1 || length == 3 && str[3] - str[0] != 2
       || length == 4 && str[3] - str[0] != 3 || str[1] < '1' || str[1] > '8' || str[0] < 'A' || str[0] > 'H'
       || str[3] < 'A' || str[3] > 'H') {
-        Interface::PrintIncorrectArrangement(str);
+        interface.PrintIncorrectArrangement(str);
         correct = true;
         continue;
       }
@@ -67,34 +67,34 @@ void Player::Arrangement(int length) {
         if (IsCorrect(str[1] - '0', str[0] - 64 + i, old_field)) {
           field.own_field[str[1] - '0'][str[0] - 64 + i].condition = "ship";
         } else {
-          Interface::PrintIncorrectArrangement(str);
+          interface.PrintIncorrectArrangement(str);
           field = old_field;
           correct = true;
           break;
         }
       }
     } else {
-      Interface::PrintIncorrectArrangement(str);
+      interface.PrintIncorrectArrangement(str);
     }
   }
 }
 
 void Player::PlayerArrangement() {
-  Interface::PrintSingleDeck(name);
+  interface.PrintSingleDeck(name);
   for (int i = 0; i < 4; ++i) {
     Arrangement(1);
   }
-  Interface::PrintField(field);
-  Interface::PrintTwoDeck(name);
+  interface.PrintField(field);
+  interface.PrintTwoDeck(name);
   for (int i = 0; i < 3; ++i) {
     Arrangement(2);
   }
-  Interface::PrintField(field);
-  Interface::PrintThreeDeck(name);
+  interface.PrintField(field);
+  interface.PrintThreeDeck(name);
   for (int i = 0; i < 2; ++i) {
     Arrangement(3);
   }
-  Interface::PrintField(field);
-  Interface::PrintFourDeck(name);
+  interface.PrintField(field);
+  interface.PrintFourDeck(name);
   Arrangement(4);
 }
